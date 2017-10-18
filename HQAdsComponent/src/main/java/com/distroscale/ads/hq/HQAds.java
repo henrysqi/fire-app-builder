48bf24402517bfd857731ebf80f72776ba0283d9
Henry Qi <henryqi222@gmail.com>
2017-10-18 14:45:18 -0700

    startend, add new real tag

--- HQAdsComponent/src/main/java/com/distroscale/ads/hq/HQAds.java
+++ HQAdsComponent/src/main/java/com/distroscale/ads/hq/HQAds.java
@@ -52,10 +52,11 @@ public class HQAds implements IAds {
     ///////////////////////////////////////////////////
 
     public static final String TAG = HQAds.class.getSimpleName();
-    private String adTag = "https://bs.serving-sys.com/Serving?cn=display&amp;c=23&amp;pl=VAST&amp;pli=22065989&amp;PluID=0&amp;pos=1254&amp;ord=7,249,131,318,107,444,220&amp;cim=1";
+//    private String adTag = "https://bs.serving-sys.com/Serving?cn=display&amp;c=23&amp;pl=VAST&amp;pli=22065989&amp;PluID=0&amp;pos=1254&amp;ord=7,249,131,318,107,444,220&amp;cim=1";
 //    private String adTag = "https://serve.eyeviewads.com/vpaid/3045ae9f.xml?vjs=1&amp;so=tr:102000;vr:128436055;rsf:16,21,25,20;asf:17,3&amp;iid=a1d09b27d4ebef6eb8c3466a64a00b5a&amp;evuid=eb51be75a2bb2cd9ed8ed7de641b8fc1&amp;aw=mr%3DWcqykgAI94cKfuTOAAA5IvMNhYw3MLEbEp_klA%26bam%3D9535840976430%26bc%3DUSD%26ssp%3DADEX%26pscr%3D10000%26bcm%3D4000%26brid%3DWcqiAwAC77-9WQoBb01NB--_ve-_vQ%26br_didt%3Dcookie%26br_pdid%3Deb51be75a2bb2cd9ed8ed7de641b8fc1%26bld%3D13323%26br_pd%3Dclutchpoints.com%26glid%3D9016921%26sf%3D17%2C3%26br_os%3Dandroid%26xpubid%3Dcom.facebook.katana%26brdcr%3D0%26fid%3D27004%26vw-sc%3Dnull%26vcr-sc%3D0.571%26usf-sc%3D1.5824877716656889%26bpmode%3DEV_VPAID_JS%26br_dv%3D4";
 //    private String adTag = "https://bs.serving-sys.com/Serving?cn=display&amp;c=23&amp;pl=VAST&amp;pli=21833538&amp;PluID=0&amp;pos=8510&amp;ord=4909727784201107456&amp;cim=1&amp;pcp=$$5522770491847021604$$&amp;pcp2=4432863%7C%7C2454551&amp;ebappid=$$$$";
 //    private String adTag = "https://serve.eyeviewads.com/vpaid/b1bd5375.xml?vjs=1&amp;so=tr:98337;vr:129074437;rsf:25,20,21,16;asf:3,6,13,17,10&amp;iid=cf1f61ead63871e5bc1d59dc65b7f803&amp;evuid=9514e68617706e81c3cf60e96dcbc9dd&amp;aw=mr%3DWcrWrAACdNgKJAbRAA5kjdjTr5FxGDvF6MnyVg%26bam%3D3766156395000%26bc%3DUSD%26ssp%3DADEX%26pscr%3D10000%26bcm%3D2500%26pmpid%3D583%26pbdlid%3D500413515899603150%26brid%3DWe-_ve-_vV4ABu-_ve-_vQoBTlDvv70J77-9Yw%26br_didt%3Dcookie%26br_pdid%3D9514e68617706e81c3cf60e96dcbc9dd%26bld%3D13323%26br_pd%3Dwfsb.com%26glid%3D9003264%26sf%3D3%2C6%2C13%2C17%2C10%26br_os%3Dandroid%26xpubid%3D7959%26brdcr%3D0%26fid%3D25275%26vw-sc%3Dnull%26vcr-sc%3D0.0%26usf-sc%3D1.0%26bpmode%3DEV_VPAID_JS%26br_dv%3D4";
+    private String adTag = "https://googleads.g.doubleclick.net/pagead/ads?client=ca-video-pub-3851171199385532&amp;slotname=9292816986&amp;ad_type=standardvideo&amp;description_url=http%3A%2F%2Flatimes.com&amp;videoad_start_delay=0";
 
     // Factory class for creating SDK objects.
     private ImaSdkFactory mSdkFactory;
@@ -88,6 +89,7 @@ public class HQAds implements IAds {
             @Override
             public void onAdError(AdErrorEvent adErrorEvent) {
                 Log.d(TAG, "ad error from adsloader");
+                startEndAd();
             }
         });
         mAdsLoader.addAdsLoadedListener(new AdsLoader.AdsLoadedListener() {
@@ -102,6 +104,7 @@ public class HQAds implements IAds {
                     @Override
                     public void onAdError(AdErrorEvent adErrorEvent) {
                         Log.d(TAG, "ad error from adsmanager");
+                        startEndAd();
                     }
                 });
                 mAdsManager.addAdEventListener(new AdEvent.AdEventListener() {
@@ -247,5 +250,12 @@ public class HQAds implements IAds {
 
         // Request the ad. After the ad is loaded, onAdsManagerLoaded() will be called.
         mAdsLoader.requestAds(request);
+    }
+
+    private void startEndAd() {
+        if (mIAdsEvents != null) {
+            mIAdsEvents.onAdSlotStarted(null);
+            mIAdsEvents.onAdSlotEnded(null);
+        }
     }
 }
